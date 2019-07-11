96d9ce72caf06d70caa1d0bf11f981f5d6dd855c
webslavik <slavik.sukhanov@gmail.com>
2019-07-11 19:29:28 +0300

    chore: init

---
+++ index.js
@@ -0,0 +1,38 @@
+const fastify = require('fastify')({ logger: true });
+const pointOfView = require('point-of-view');
+const static = require('fastify-static');
+const handlebars = require('handlebars');
+const path = require('path');
+
+// plugins
+fastify.register(pointOfView, {
+  engine: {
+    handlebars,
+  },
+  options: {
+    partials: {
+      header: './views/partials/header.hbs',
+    }
+  }
+});
+
+fastify.register(static, {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/',
+});
+
+// routes
+fastify.get('/', async (request, reply) => {
+  reply.view('/views/index.hbs', { });
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
